c621d63f7987548ccd40b73895f6cd7c02e4841a
ilanribeiro <ilanribeiro@gmail.com>
2021-06-20 16:33:44 -0300

    Adiciona arquivo com informações sobre categorias e nomes da apis utilizadas.

---
+++ src/services/seeds.js
@@ -0,0 +1,10 @@
+export const stateAPIs = [
+  { key: 'ML', text: 'Mercado Livre', value: 'ML' },
+  { key: 'BSCP', text: 'Buscapé', value: 'BSCP' },
+];
+
+export const stateCategories = [
+  { key: 'geladeira', text: 'Geladeira', value: 'geladeira' },
+  { key: 'celular', text: 'Celular', value: 'celular' },
+  { key: 'tv', text: 'Tv', value: 'tv' },
+];
