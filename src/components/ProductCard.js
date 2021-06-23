b91e47d7d3dd2139af28674c87cd2d2490bb50d4
ilanribeiro <ilanribeiro@gmail.com>
2021-06-23 06:20:04 -0300

    Cira o componente ProductCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,10 +1,58 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { Card, Icon, Image, Container } from 'semantic-ui-react';
+import Context from '../context/Context';
+
+import './Components.css';
 
 function ProductCard() {
+  const { results, web, category } = useContext(Context);
+
+  const numberFormat = (value) =>
+  new Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  }).format(value);
+
   return (
-    <div>
-      <h3>Product Card</h3>
-    </div>
+    <Container fluid className="card-container">
+      <Card.Group className="card-ui-container">
+        { results.map((product, index) => (
+          <Card
+            key={ index }
+            style={ {
+              maxWidth: 200,
+              margin: '20px 10px',
+              boxShadow: '10px 10px 4px rgba(0, 0, 0, 10)',
+            } }>
+
+            <Image
+              src={ product.thumbnail }
+              wrapped
+              ui={ true }
+              size="small"
+              centered
+              // as = 'a'
+            />
+
+            <Card.Content>
+              <Card.Header>{ product.title }</Card.Header>
+              <Card.Meta>
+                <span className='date'>Categoria: { category }</span>
+              </Card.Meta>
+              <Card.Description>
+                { numberFormat(product.price) }
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a href='@'>
+                <Icon name='user' />
+                { web }
+              </a>
+            </Card.Content>
+          </Card>
+        ))}
+      </Card.Group>
+    </Container>
   )
 };
 
