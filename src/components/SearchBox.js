f6518679f792a279a14108ba48b21e7c417fdd24
ilanribeiro <ilanribeiro@gmail.com>
2021-06-23 06:19:50 -0300

    Cria o componente searchBox

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -1,10 +1,65 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import Context from '../context/Context';
+import { Dropdown, Icon, Input, Container } from 'semantic-ui-react';
+import { stateAPIs, stateCategories } from '../services/seeds';
+import switchAPI from '../services/api';
+
+import './Components.css';
 
 function SearchBox() {
+  const {
+    web,
+    setWeb,
+    category,
+    setCategory,
+    searchTerm,
+    setSearchTerm,
+    setResults,
+ } = useContext(Context);
+
+  const handleFilter = async (web, category, searchTerm) => {
+    const productResults = await switchAPI(web, category, searchTerm);
+    setResults(productResults);
+  };
+
   return (
-    <div>
-      <h2>Search Box</h2>
-    </div>
+    <Container fluid className="search-container" textAlign="center">
+      <Dropdown
+        className="search-input"
+        id='api'
+        placeholder='Web'
+        search
+        selection
+        options={ stateAPIs }
+        onChange={ (e) => setWeb(e.target.innerText) }
+      />
+
+      <Dropdown
+        className="search-input"
+        id='category'
+        placeholder='Categoria'
+        search
+        selection
+        options={ stateCategories }
+        onChange={ (e) => setCategory(e.target.textContent) }
+      />
+
+      <Input
+        icon={
+          <Icon
+            name='search'
+            inverted
+            circular
+            link
+            onClick={ (e) => handleFilter(web, category, searchTerm) }
+          />
+        }
+        className="search-input"
+        placeholder='O que está buscando?'
+        value={ searchTerm }
+        onChange={ (e) => setSearchTerm(e.target.value)}
+      />
+    </Container>
   )
 };
 
